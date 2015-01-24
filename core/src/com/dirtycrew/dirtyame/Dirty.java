eccb41480b1e1c23349bd278bea864fe4c6de26e
Kevin.Schroeder <kevin.schroeder@target.com>
2015-01-24 03:17:53 -0600

    First iteration of physics

--- core/src/com/dirtycrew/dirtyame/Dirty.java
+++ core/src/com/dirtycrew/dirtyame/Dirty.java
@@ -8,23 +8,58 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class Dirty extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
-	private static Player player = new Player();
+	private static Player player;
 	private OrthographicCamera camera;
 	Map map;
+	World world;
+	Body playerBody;
+	Body blockBody;
+	Box2DDebugRenderer debugRenderer;
 
 	@Override
 	public void create () {
 		DLog.debug("ScreenWidth: {} ScreenHeight:{}", Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		DLog.error("ScreenWidth: {} ScreenHeight:{}", Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
+		debugRenderer = new Box2DDebugRenderer();
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 640, 480);
 		camera.update();
+		world = new World(new Vector2(0, 0), true);
+		// First we create a body definition
+		BodyDef playerBodyDef = new BodyDef();
+		playerBodyDef.type = BodyDef.BodyType.DynamicBody;
+		playerBodyDef.position.set(0, 0);
+		playerBody = world.createBody(playerBodyDef);
+		PolygonShape playerBox = new PolygonShape();
+		playerBox.setAsBox(25, 25);
+
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = playerBox;
+		fixtureDef.density = 0.0f;
+		fixtureDef.friction = 0.0f;
+		fixtureDef.restitution = 1f; // Make it bounce a little bit
+
+		playerBody.createFixture(fixtureDef);
+
+		player = new Player(playerBody);
+
+		BodyDef boxBodyDef = new BodyDef();
+		playerBodyDef.type = BodyDef.BodyType.StaticBody;
+		boxBodyDef.position.set(200, 0);
+		blockBody = world.createBody(boxBodyDef);
+		PolygonShape blockBox = new PolygonShape();
+		blockBox.setAsBox(25, 25);
+		blockBody.createFixture(blockBox, 0.0f);
+		blockBox.dispose();
+		playerBox.dispose();
 
 		batch = new SpriteBatch();
 
@@ -71,6 +106,9 @@ public class Dirty extends ApplicationAdapter {
 
 		batch.setProjectionMatrix(camera.projection);
 		batch.setTransformMatrix(camera.view);
+		debugRenderer.render(world, camera.combined);
+		world.step(1/60f, 6, 2);
+
 		batch.begin();
 		batch.draw(img, player.getPosition().x, player.getPosition().y, player.PLAYER_WIDTH, player.PLAYER_HEIGHT);
 

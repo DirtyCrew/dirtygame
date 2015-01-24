37b11ffde6307348cc79e8d9607cc25af8837a0c
Eugene Sturm <sturm049@gmail.com>
2015-01-23 21:43:53 -0600

    first

---
+++ desktop/src/com/dirtycrew/dirtyame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dirtycrew.dirtyame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dirtycrew.dirtyame.Dirty;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Dirty(), config);
+	}
+}

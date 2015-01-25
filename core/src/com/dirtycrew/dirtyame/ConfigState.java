ff9a62362ed0efe7f859cc4b08a52a071a2f6da3
Jared Poetter <poet0007@umn.edu>
2015-01-25 12:50:26 -0600

    does not work player picker

--- core/src/com/dirtycrew/dirtyame/ConfigState.java
+++ core/src/com/dirtycrew/dirtyame/ConfigState.java
@@ -1,5 +1,11 @@
 package com.dirtycrew.dirtyame;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 /**
  * Created by sturm on 1/25/15.
  */
@@ -17,12 +23,26 @@ public class ConfigState implements IGameState {
 
     @Override
     public void update(Dirty game, float delta) {
-        game.setNumPlayers(1);
-        game.gameManager.changeState(GameManager.GameState.Start);
+        if (Gdx.input.isKeyPressed(Input.Keys.NUM_1)){
+            game.setNumPlayers(1);
+            game.gameManager.changeState(GameManager.GameState.Start);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.NUM_2)){
+            game.setNumPlayers(2);
+            game.gameManager.changeState(GameManager.GameState.Start);
+        }
     }
 
     @Override
     public void render(Dirty game) {
-
+        game.batch.begin();
+        Gdx.gl.glClearColor(135 / 255f, 206 / 255f, 235 / 255f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Texture texture = new Texture("number_of_players_screen.png");
+        Sprite backgroundSprite = new Sprite(texture);
+        backgroundSprite.setSize(650, 480);
+        backgroundSprite.draw(game.batch);
+        game.batch.end();
     }
 }
